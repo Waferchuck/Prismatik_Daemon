063a18b775624c75e1aedba3ead175bfaa046454
Zhiyang Li <dllizhiyang@gmail.com>
2022-11-13 20:44:06 -0500

    Initial checkin

---
+++ Prismatik_Daemon.cpp
@@ -0,0 +1,76 @@
+// Prismatik_Daemon.cpp : This application will first read in the COM port number from main.conf 
+// and then keeps monitoring the COM status in backgroud and restart the Prismatik.exe if COM is avaliable.
+
+#pragma comment( linker, "/subsystem:windows /entry:mainCRTStartup")
+#include<windows.h>
+#include<stdio.h>
+
+using namespace std;
+
+extern int read_conf_COM_port_string(char** port_num);
+
+
+void restart_prismatic(void)
+{
+    system("taskkill /f /t /im Prismatik.exe");
+    printf("Prismatik.exe has been killed\nRestarting \n");
+    Sleep(3000);
+    ShellExecute(NULL, L"open", L"C:\\Program Files\\Prismatik\\Prismatik.exe", NULL, NULL, SW_SHOW);
+    HINSTANCE ShellExecute(
+        HWND hwnd,
+        LPCTSTR lpOperation,
+        LPCTSTR lpFile,
+        LPCTSTR lpParameters,
+        LPCTSTR lpDirectory,
+        INT nShowCmd
+    );
+    printf("\nPrismatik.exe has been restarted \n");
+}
+
+int main()
+{
+    HANDLE hComm;
+
+    //char port_name[256] = "\\\\\\\\.\\\\";
+    char port_name[256] = "\\\\.\\";
+    char* port_str_read = NULL;
+    if (read_conf_COM_port_string(&port_str_read) == 0)
+    {
+        printf("read back port_str is %s\n", port_str_read);
+        strcat(port_name, port_str_read);
+    }
+    else
+    {
+        printf("Error readin main.conf, EXIT");
+        return 1;
+    }
+    printf("full port_str is %s\n", port_name);
+
+    while (true)
+    {
+        //printf("full port_str is %s\n", port_name);
+
+        hComm = CreateFileA(port_name,  //port name
+            GENERIC_READ | GENERIC_WRITE,   //Read/Write
+            0,                              // No Sharing
+            NULL,                           // No Security
+            OPEN_EXISTING,                  // Open existing port only
+            0,                              // Non Overlapped I/O
+            NULL);                          // Null for Comm Devices
+
+        if (hComm == INVALID_HANDLE_VALUE)
+            printf("Cannot open port %s, probably in use or does not exist\n", port_str_read);
+        else
+        {
+            printf("opening %s successfully, port idle, kick off Prismatik.exe\n", port_str_read);
+            CloseHandle(hComm);//free the port
+            Sleep(2000);
+            restart_prismatic();
+        }
+        Sleep(3000); //check every 3 seconds
+    }
+
+    return 0;
+}
+
+
---
+++ get_port_name.cpp
@@ -0,0 +1,94 @@
+// get_port_number.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include "./ini.h"
+
+#pragma comment(lib, "Advapi32.lib")
+
+typedef struct
+{
+    int device;
+    const char* serial_number;
+} configuration;
+
+static int handler(void* user, const char* section, const char* name,
+    const char* value)
+{
+    configuration* pconfig = (configuration*)user;
+
+#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("Adalight", "SerialPort")) {
+        pconfig->serial_number = _strdup(value);
+    }
+    else {
+        return 0;
+    }
+    return 1;
+}
+
+#if 0
+int read_conf_COM_port_value(int* port_num)
+{
+
+    char* user_name = getenv("username");
+    printf("user name is %s \n", user_name);
+    char main_conf_path[256];
+    strcpy(main_conf_path, "C:\\Users\\");
+    strcat(main_conf_path, user_name);
+    strcat(main_conf_path, "\\Prismatik\\main.conf");
+    printf("path is %s \n", main_conf_path);
+
+    configuration config;
+
+    if (ini_parse(main_conf_path, handler, &config) < 0) {
+        printf("Can't load '%s\n", main_conf_path);
+        return 1;
+    }
+    printf("Config loaded from %s : serial_number is %s\n", main_conf_path, config.serial_number);
+    int number;
+    sscanf(config.serial_number, "s%d", &number);
+    printf("number is %d \n", number);
+    return 0;
+
+}
+#endif
+
+int read_conf_COM_port_string(char** port_num)
+{
+
+    char* user_name = getenv("username");
+    // printf("user name is %s \n", user_name);
+    char main_conf_path[256];
+    strcpy(main_conf_path, "C:\\Users\\");
+    strcat(main_conf_path, user_name);
+    strcat(main_conf_path, "\\Prismatik\\main.conf");
+    printf("main.conf path is %s \n", main_conf_path);
+
+    configuration config;
+
+    if (ini_parse(main_conf_path, handler, &config) < 0) {
+        printf("Can't load '%s\n", main_conf_path);
+        return 1;
+    }
+    //printf("Config loaded from %s : serial_number is %s\n", main_conf_path, config.serial_number);
+    *port_num = (char*)malloc(100);
+    strcpy(*port_num, config.serial_number);
+    return 0;
+
+}
+
+int test()
+{
+    //int port_number;
+    //read_conf_COM_port_value(&port_number);
+    char* port_str = NULL;
+    read_conf_COM_port_string(&port_str);
+    //printf("return is %d\n", port_number);
+    printf("return str is %s\n", port_str);
+    return 0;
+}
+
